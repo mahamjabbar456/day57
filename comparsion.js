8743f116ee09d49088dca7b94cbba5995fced415
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-05-05 09:56:43 +0500

    i learn about comparsion , conditional statement and ternary operator.

---
+++ comparsion.js
@@ -0,0 +1,19 @@
+// Boolean comparsion
+console.log(5 > 1);
+console.log(2 <= 1);
+let num1 = 2;
+let num2 = 3;
+console.log(num1 == num2);
+// String Comparsion
+console.log("a" == "a");
+console.log("Elephant" > "Lion");
+console.log("Twinkle" < "Little");
+console.log(null == undefined);
+console.log(5 > 4);
+console.log("apple" > "pineapple");
+console.log("2" > "12");
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
+export {};
---
+++ conditional.ts
@@ -0,0 +1,41 @@
+let year:number = 2000;
+if(year %4 == 0){
+    console.log(year," It is a leap year")
+}else{
+    console.log(year," It is not a leap year")
+}
+
+// Conditional operator
+let age = 150;
+let accessAllow = age >18 ? "You can drive":"You cannot drive.";
+console.log(accessAllow);
+
+let message = (age < 3) ? 'Hi, baby!' :
+  (age < 18) ? 'Hello!' :
+  (age < 100) ? 'Greetings!' :
+  'What an unusual age!';
+
+console.log(message);
+
+let result;
+let a = 5,b = 5;
+(a+b<4) ? result = "Below" : result = "Over";
+console.log(result);
+// Rewrite if else using multiple ternary operator
+// let message;
+
+// if (login == 'Employee') {
+//   message = 'Hello';
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+
+let login = "Director";
+(login == "Employee")?console.log("Hello"):
+(login == 'Director')?console.log("Greetings"):
+(login == '')?console.log("No Login"):
+console.log("");
